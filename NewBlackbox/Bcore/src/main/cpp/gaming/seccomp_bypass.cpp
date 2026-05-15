33d4514166d59082acd2413c128f6030b0db8f93
OneCoreServer <mexk18310@gmail.com>
2026-05-15 18:07:27 +0530

    Fix NDK seccomp compile compatibility in gaming native

--- NewBlackbox/Bcore/src/main/cpp/gaming/seccomp_bypass.cpp
+++ NewBlackbox/Bcore/src/main/cpp/gaming/seccomp_bypass.cpp
@@ -1,14 +1,24 @@
 #include <android/log.h>
+#include <errno.h>
 #include <sys/prctl.h>
 #include <linux/seccomp.h>
 
 #define SLOGI(...) __android_log_print(ANDROID_LOG_INFO, "SeccompBypass", __VA_ARGS__)
 
+#ifndef SECCOMP_MODE_DISABLED
+#define SECCOMP_MODE_DISABLED 0
+#endif
+
 bool init_seccomp_bypass() {
 #ifdef PR_SET_SECCOMP
+    errno = 0;
     int ret = prctl(PR_SET_SECCOMP, SECCOMP_MODE_DISABLED, 0, 0, 0);
-    SLOGI("prctl(PR_SET_SECCOMP, DISABLED) result=%d", ret);
-    return ret == 0;
+    if (ret == 0) {
+        SLOGI("prctl(PR_SET_SECCOMP, DISABLED) succeeded");
+        return true;
+    }
+    SLOGI("prctl(PR_SET_SECCOMP, DISABLED) failed, errno=%d", errno);
+    return false;
 #else
     SLOGI("PR_SET_SECCOMP not supported on this build");
     return false;
