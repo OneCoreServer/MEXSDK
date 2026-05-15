a1aeef800dd68de45c33fec0c1ecb21aa53c885b
OneCoreServer <mexk18310@gmail.com>
2026-05-15 18:30:40 +0530

    Make seccomp bypass source header-independent for ndk-build

--- NewBlackbox/Bcore/src/main/cpp/gaming/seccomp_bypass.cpp
+++ NewBlackbox/Bcore/src/main/cpp/gaming/seccomp_bypass.cpp
@@ -2,18 +2,17 @@
 #include <errno.h>
 #include <sys/prctl.h>
 
-#if __has_include(<linux/seccomp.h>)
-#include <linux/seccomp.h>
-#endif
-
 #define SLOGI(...) __android_log_print(ANDROID_LOG_INFO, "SeccompBypass", __VA_ARGS__)
+
+#ifndef PR_SET_SECCOMP
+#define PR_SET_SECCOMP 22
+#endif
 
 #ifndef SECCOMP_MODE_DISABLED
 #define SECCOMP_MODE_DISABLED 0
 #endif
 
 bool init_seccomp_bypass() {
-#ifdef PR_SET_SECCOMP
     errno = 0;
     int ret = prctl(PR_SET_SECCOMP, SECCOMP_MODE_DISABLED, 0, 0, 0);
     if (ret == 0) {
@@ -22,8 +21,4 @@ bool init_seccomp_bypass() {
     }
     SLOGI("prctl(PR_SET_SECCOMP, DISABLED) failed, errno=%d", errno);
     return false;
-#else
-    SLOGI("PR_SET_SECCOMP not supported on this build");
-    return false;
-#endif
 }
